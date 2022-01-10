3cee910e28588ba0c673b75488b079ee0cadba6c
jihoon <eezzee@reconers.com>
2022-01-10 22:11:23 +0900

    라우터 - 화면 처리기 만들기 실습

--- dist/index.833266ea.js
+++ dist/index.833266ea.js
@@ -529,14 +529,31 @@ const content = document.createElement('div');
 //   ajax.open( 'GET', url, false );
 //   ajax.send();
 // }
-const getData = function(url) {
+function getData(url) {
     ajax.open('GET', url, false);
     ajax.send();
     return JSON.parse(ajax.response);
-};
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul'); // ul 태그를 만든다.
-window.addEventListener('hashchange', function() {
+}
+// 라우터란 - 중계기 (화면이 여러개가 있다고 하면 A상태 B상태를 상황에 맞게 보여주는 것)
+// 글 목록화면의 코드를 함수로 묶어주어야 한다. 
+// 목록 화면을 보여주는 함수
+function newsFeed() {
+    const newsFeed1 = getData(NEWS_URL); // 데이터를 가져오는 코드
+    const newsList = [];
+    newsList.push(`<ul>`);
+    for(let i = 0; i < 10; i++)// DOM API 제거실습
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed1[i].id}"> 
+          ${newsFeed1[i].title} (${newsFeed1[i].comments_count}) 
+        <a>
+      </li>
+    `);
+    newsList.push(`</ul>`);
+    container.innerHTML = newsList.join('');
+}
+// 뉴스 상세 부르는 함수
+function newsDetail() {
     const id = location.hash.substring(1);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
@@ -547,33 +564,28 @@ window.addEventListener('hashchange', function() {
     // 사용자는 새로운 화면으로 진입했다고 느낄 것
     container.innerHTML = `
     <h1>${newsContent.title}</h1>
-
     <div>
       <a href="#">목록으로</a>
     </div>
   `; // 기존 내용이 전부 제거, div#root 하위요소에 '' 빈 내용 삽입
-});
-// 다음 단계 - // 목록 구조도 똑같은 구조여야 한다.
-// 문자열 자체를 for만으로 만들 수 없다. 
-// 배열을 이용하고자 한다. (자주 쓰이는 테크닉)
-// 1. 빈 배열을 만들고
-// 2. 배열.push('<ul>'); 
-//        <li> <a></a> </li> -> li구조 반복되게
-// 3. 마지막으로 배열.push('</ul>');
-// 4. 최종적으로 innerHTMl 전에 배열.jon(''); 하나의 문자열로 만들어준다.
-const newsList = [];
-newsList.push(`<ul>`);
-for(let i = 0; i < 10; i++)// DOM API 제거실습
-newsList.push(`
-    <li>
-      <a href="#${newsFeed[i].id}"> 
-        ${newsFeed[i].title} (${newsFeed[i].comments_count}) 
-      <a>
-    </li>
-  `);
-newsList.push(`</ul>`);
-console.log(newsList.join(''));
-container.innerHTML = newsList.join(''); // 라우터란 - 중계기 (화면이 여러개가 있다고 하면 A상태 B상태를 상황에 맞게 보여주는 것)
+}
+// 라우터 - 화면이 전환되어야 할 때를 판단
+// 기존에 hashchange를 통해 화면 전환 트리거를 사용했음
+// 해시가 바뀌면 무조건 글 내용을 보는 것이다 (지금까지는)
+// 해시가 바뀔때마다 router함수가 호출되도록 변경
+function router() {
+    // 시작하자마자 게시글을 한번 보여주어야 한다.
+    // 실제로 화면을 전화하는 것이 목적
+    const routePath = location.hash;
+    // 초기 hash값이 없으면 newsFeed 함수를 호출하고
+    // '#' 만 들어있을 경우에는 빈값을 반환함
+    if (routePath === '') newsFeed();
+    else newsDetail();
+}
+// 글 내용 화면 - 함수로 구성됨
+// 이벤트 핸들러함수가 묶여 있을 경우 밖으로 빼낼 방법이 없다..? (무슨 뜻. 익명함수로 되어있으면 부를 방법이 없다.)
+window.addEventListener('hashchange', router);
+router(); // 초기에 router 함수를 호출
 
 },{}]},["aJTy4","lzYRN"], "lzYRN", "parcelRequire94c2")
 
