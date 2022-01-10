db61e77d8d9ebc70a8dd6c1f8e94b86d59f3695f
jihoon <eezzee@reconers.com>
2022-01-10 21:52:41 +0900

    구조 구축

--- dist/index.833266ea.js
+++ dist/index.833266ea.js
@@ -540,26 +540,40 @@ window.addEventListener('hashchange', function() {
     const id = location.hash.substring(1);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
-    content.appendChild(title);
-    title.innerHTML = newsContent.title;
+    // content.appendChild(title); // 추가하는 코드만 있다.
+    // title.innerHTML = newsContent.title; 
+    // 내용화면으로 진입 시 목록화면을 지워주는 코드
+    // 목록화면을 지우고 새로운 화면(제목 + 콘텐츠 + 목록으로 버튼) UI를 그린다.
+    // 사용자는 새로운 화면으로 진입했다고 느낄 것
+    container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `; // 기존 내용이 전부 제거, div#root 하위요소에 '' 빈 내용 삽입
 });
-for(let i = 0; i < 10; i++){
-    const div = document.createElement('div'); // 임시로 div를 만들어 안에 li태그를 생성, 사용한다.
-    // 문자열을 가지고 html 구조를 만드는 것이 파악하기 쉽다.
-    div.innerHTML = `
+// 다음 단계 - // 목록 구조도 똑같은 구조여야 한다.
+// 문자열 자체를 for만으로 만들 수 없다. 
+// 배열을 이용하고자 한다. (자주 쓰이는 테크닉)
+// 1. 빈 배열을 만들고
+// 2. 배열.push('<ul>'); 
+//        <li> <a></a> </li> -> li구조 반복되게
+// 3. 마지막으로 배열.push('</ul>');
+// 4. 최종적으로 innerHTMl 전에 배열.jon(''); 하나의 문자열로 만들어준다.
+const newsList = [];
+newsList.push(`<ul>`);
+for(let i = 0; i < 10; i++)// DOM API 제거실습
+newsList.push(`
     <li>
       <a href="#${newsFeed[i].id}"> 
         ${newsFeed[i].title} (${newsFeed[i].comments_count}) 
       <a>
     </li>
-  `;
-    // div에 추가된 li만 사용하는 방법
-    // 1. div.children[0]
-    // 2. div.firstElementChild
-    ul.appendChild(div.firstElementChild);
-}
-container.appendChild(ul); // ul태그를 root 자식요소로 만든다.
-container.appendChild(content); // 버그 발생(기존 제목은 그대로 남아있음)
+  `);
+newsList.push(`</ul>`);
+console.log(newsList.join(''));
+container.innerHTML = newsList.join(''); // 라우터란 - 중계기 (화면이 여러개가 있다고 하면 A상태 B상태를 상황에 맞게 보여주는 것)
 
 },{}]},["aJTy4","lzYRN"], "lzYRN", "parcelRequire94c2")
 
